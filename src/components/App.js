e26831ff2eb02b04c3f246ebf8c74f7a5fdc9fc4
Carlos Maciel <carloshfmaciel@MacBook-Pro-de-Carlos.local>
2019-01-23 01:59:45 -0200

    usando props

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PlacarContainer from './PlacarContainer';
+
+const dados = {
+    partida: {
+        estadio: "Marcanã/RJ",
+        data: "01/06/2016",
+        horario: "19hs"
+    },
+    casa: {
+        nome: "Vasco"
+    },
+    visitante: {
+        nome: "Flamengo"
+    }
+};
+
+export default class App extends React.Component {
+    render(){
+        return (
+                <PlacarContainer partida={dados.partida} 
+                                 casa={dados.casa}
+                                 visitante={dados.visitante} />
+            );
+    }
+}
